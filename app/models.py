b7245250a4aad3b79a287bd3991a0ef4722cca11
Adamwcc <adamwcc@gmail.com>
2025-11-18 19:38:41 +0800

    Fix: Import Boolean type in models.py

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Enum, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, Enum, DateTime, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 from .database import Base
 import enum
