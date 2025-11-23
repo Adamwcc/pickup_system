c802ab2b2404dc3f5cf03dd4e5cdc95c78df05b3
Adamwcc <adamwcc@gmail.com>
2025-11-23 21:05:43 +0800

    Fix: Re-add TeacherCreate schema

--- app/crud.py
+++ app/crud.py
@@ -31,23 +31,6 @@ def create_teacher(db: Session, user: schemas.TeacherCreate):
     db.commit()
     db.refresh(db_user)
     return db_user
-# ... (檔案上方原有的 get_user_by_phone, create_user 函式保持不變) ...
-
-def create_teacher(db: Session, user: schemas.TeacherCreate):
-    """
-    建立一個具有指定角色的使用者（老師或管理員）。
-    """
-    hashed_password = security.get_password_hash(user.password)
-    db_user = models.User(
-        phone_number=user.phone_number,
-        hashed_password=hashed_password,
-        full_name=user.full_name,
-        role=user.role  # 使用從請求中傳入的角色
-    )
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
 
 # ... (檔案上方原有的函式保持不變) ...
 
